290b1c1ab7777505faab23981aa5239eecf054d4
nbyrd9 <nianibyrd@gmail.com>
2021-05-25 07:39:33 -0400

    commit tree 5cb3a0e8a1180fb10c6b20f5bb1a97900284b765

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -9,7 +10,9 @@ const Home = () => {
   );
 };
 
-ReactDOM.render(
-  <Home />,
+ReactDOM.render((
+  <Router>
+    <Route path="/" component={Home} />
+  </Router>),
   document.getElementById('root')
 );
